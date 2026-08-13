1a9b9a3e0d8e58e411eab7a95e0d6c4f75f9d236
Mohamed Anees M K <138788670+mohamedaneesmk@users.noreply.github.com>
2026-08-13 17:12:48 +0530

    Time: 20 ms (66.18%), Space: 45.8 MB (63.23%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -14,30 +14,10 @@ class Solution {
                 continue;
             }
 
-            // Minimum possible sum is greater than target
-            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
-                break;
-            }
-
-            // Maximum possible sum is smaller than target
-            if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) {
-                continue;
-            }
-
             for (int j = i + 1; j < n - 2; j++) {
 
                 // Skip duplicate j
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-
-                // Minimum possible sum for this j
-                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
-                    break;
-                }
-
-                // Maximum possible sum for this j
-                if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) {
                     continue;
                 }
 
@@ -63,18 +43,21 @@ class Solution {
                         left++;
                         right--;
 
-                        // Skip duplicate left values
-                        while (left < right && nums[left] == nums[left - 1]) {
+                        // Skip duplicate left
+                        while (left < right &&
+                               nums[left] == nums[left - 1]) {
                             left++;
                         }
 
-                        // Skip duplicate right values
-                        while (left < right && nums[right] == nums[right + 1]) {
+                        // Skip duplicate right
+                        while (left < right &&
+                               nums[right] == nums[right + 1]) {
                             right--;
                         }
 
                     } else if (sum < target) {
                         left++;
+
                     } else {
                         right--;
                     }
