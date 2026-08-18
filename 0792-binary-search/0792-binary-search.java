ecb1723ee4a92c0eb4b147db24a20af07560192c
Mohamed Anees M K <138788670+mohamedaneesmk@users.noreply.github.com>
2026-08-18 12:04:47 +0530

    Time: 0 ms (100%), Space: 48.4 MB (31.76%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,20 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] > target) {
-                // target is smaller, search left half
-                end = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                // target is larger, search right half
-                start = mid + 1;
+                right = mid - 1;
             }
         }
+
         return -1;
     }
 }
