0b8b7aafffe2ea96b17da004ba0e276b7dd216d5
Mohamed Anees M K <138788670+mohamedaneesmk@users.noreply.github.com>
2026-08-24 18:59:57 +0530

    Time: 0 ms (100%), Space: 48.6 MB (14.19%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,18 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
+        int result = findIndex(nums, 0, nums.length - 1, target);
+        return result;
+    }
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+    private static int findIndex(int[] nums, int low, int high, int target) {
+        if (low > high) return -1;
+
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return findIndex(nums, mid + 1, high, target);
+        } else {
+            return findIndex(nums, low, mid - 1, target);
         }
-
-        return -1;
     }
 }
