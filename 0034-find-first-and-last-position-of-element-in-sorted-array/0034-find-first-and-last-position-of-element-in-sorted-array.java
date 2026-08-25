b2ccf6e1ad965a7cdbf55b93f7860c35f4001428
Mohamed Anees M K <138788670+mohamedaneesmk@users.noreply.github.com>
2026-08-25 20:14:09 +0530

    Time: 0 ms (100%), Space: 48.2 MB (31.95%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,46 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findBound(nums, target, true);
-        if (first == -1) return new int[]{-1, -1};
-        int last = findBound(nums, target, false);
-        return new int[]{first, last};
+        int n = nums.length;
+        int lowerBound = findLowerBound(nums, n, target);
+        
+        if (lowerBound == n || nums[lowerBound] != target) {
+            return new int[]{-1, -1};
+        }
+
+        return new int[]{lowerBound, findUpperBound(nums, n, target) - 1};
     }
 
-    private int findBound(int[] nums, int target, boolean findFirst) {
+    public static int findLowerBound(int[] nums, int n, int x) {
         int low = 0, high = nums.length - 1;
-        int result = -1;
+        int ans = n;
+
         while (low <= high) {
             int mid = (low + high) / 2;
-            if (nums[mid] == target) {
-                result = mid;
-                if (findFirst) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
+            if (nums[mid] >= x) {
+                ans = mid;
                 high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return result;
+
+        return ans;
+    }
+
+    public static int findUpperBound(int[] nums, int n, int x) {
+        int low = 0, high = nums.length - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
