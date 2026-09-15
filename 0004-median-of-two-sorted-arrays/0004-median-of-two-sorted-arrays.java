8b4f65e142dc2f2e1fe1ee271d450e767e0327e6
Mohamed Anees M K <138788670+mohamedaneesmk@users.noreply.github.com>
2026-09-15 19:06:57 +0530

    Time: 1 ms (100%), Space: 48.9 MB (51.91%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Ensure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        int n = nums1.length;
-        int m = nums2.length;
-        int total = n + m;
-        int half = (total + 1) / 2; // size of left partition
+        int[] temp = new int[n1 + n2];
+        int x = 0;
 
-        int low = 0, high = n;
+        int i = 0;
+        int j = 0;
 
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = half - cut1;
-
-            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int r1 = (cut1 == n) ? Integer.MAX_VALUE : nums1[cut1];
-            int r2 = (cut2 == m) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1) {
-                if (total % 2 == 0) {
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                } else {
-                    return Math.max(l1, l2);
-                }
-            } else if (l1 > r2) {
-                high = cut1 - 1;
+        while (i < n1 && j < n2) {
+            if (nums1[i] <= nums2[j]) {
+                temp[x++] = nums1[i];
+                i++;
             } else {
-                low = cut1 + 1;
+                temp[x++] = nums2[j];
+                j++;
             }
         }
 
-        return -1.0; // shouldn't reach here given valid input
+        while (i < n1) {
+            temp[x++] = nums1[i];
+            i++;
+        }
+
+        while (j < n2) {
+            temp[x++] = nums2[j];
+            j++;
+        }
+
+        int mid = temp.length / 2;
+
+        if (temp.length % 2 == 0) {
+            return (temp[mid - 1] + temp[mid]) / 2.0;
+        } else {
+            return temp[mid];
+        }
     }
 }
